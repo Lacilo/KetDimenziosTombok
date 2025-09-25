a28c2dbb972d3d7d5f1d042ed156ebb9e1c1bd57
Lacilo <szaszfail@kkszki.hu>
2025-09-25 09:09:07 +0200

    Add project files.

---
+++ KétDimenziósTömbök/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace KetDimenziosTombok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // HA EGY ELEM 1 SORSZÁMMAL AZONOSÍHATÓ A TÖMBBEN AKKOR AZ EGYDMINENZIÓS TÖMB (ELEMEK EGYSZERŰ SORA)
+
+            // HA AZ AZONOSÍTÁSHOZ 2 INDEXET HASZNÁLUNK AKKOR 2 DIMENZIÓS TÖMB (OLYAN MINT EGY TÁBLÁZAT)
+            int[,] tablazat = new int[2, 3];
+
+        }
+    }
+}
